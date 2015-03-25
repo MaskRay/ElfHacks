8988f2f866a576c9131d0bc522144717b0e7028a
Ray Song <i@maskray.me>
2015-03-25 09:41:15 +0800

    add ld-execstack & add common.exp

--- ld-execstack/a.c
+++ ld-execstack/a.c
@@ -1,1 +1,3 @@
-int main() {}
+// NX bit also applies to other memory regions such as .data though there is no execution permission in the ELF
+char data[1] = {0xc3}; // ret instruction in .data
+int main() { ((void(*)())data)(); }
